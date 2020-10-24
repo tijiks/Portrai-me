b3ae6a2931a8d0965d85893c7e133213578472be
tijiks <73350878+tijiks@users.noreply.github.com>
2020-10-24 20:19:52 +0200

    Update navHover.js

--- navHover.js
+++ navHover.js
@@ -1,8 +1,1 @@
-'use strict'
-    function colorCh(obj) {
-            if (obj.style.color == 'black') {
-                obj.style.color = 'whitesmoke';
-            } else {
-                obj.style.color = 'black';
-            }
-        }
+'use strict' function colorCh(obj) {if (obj.style.color == 'black') { obj.style.color = 'whitesmoke';} else {obj.style.color = 'black';}}
